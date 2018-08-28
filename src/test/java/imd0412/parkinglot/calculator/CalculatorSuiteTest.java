ee05c3457653b57d8fc576e92e7cc4c40ea54020
gabrielqap <gabrielqap@hotmail.com>
2018-08-28 16:51:08 -0300

    Classe suite atualizado

---
+++ src/test/java/imd0412/parkinglot/calculator/CalculatorSuiteTest.java
@@ -0,0 +1,42 @@
+package imd0412.parkinglot.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	CalculatorTest.class,
+	CalculatorExceptionTest.class
+})
+
+public class CalculatorSuiteTest {
+/*
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+*/
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
